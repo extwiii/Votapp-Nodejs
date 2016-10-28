09d2dd885e7426915f561db004b4732f77ae2af3
extwiii <bcagiran@hotmail.com>
2016-10-28 21:08:06 +0100

    updating working directories

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/votapp');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -18,12 +24,28 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,58 @@
 var express = require('express');
+var passport = require('passport');
+var Account = require('../models/account');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/', function(req, res) {
+  res.render('index', {
+    user: req.user
+  });
+});
+
+router.get('/register', function(req, res) {
+  res.render('register', {});
+});
+
+router.post('/register', function(req, res, next) {
+  Account.register(new Account({
+    username: req.body.username
+  }), req.body.password, function(err, account) {
+    if (err) {
+      return res.render('register', {
+        error: err.message
+      });
+    }
+
+    passport.authenticate('local')(req, res, function() {
+      req.session.save(function(err) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect('/');
+      });
+    });
+  });
+});
+
+
+router.get('/login', function(req, res) {
+  res.render('login', {
+    user: req.user
+  });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/ping', function(req, res) {
+  res.status(200).send("pong!");
 });
 
 module.exports = router;
